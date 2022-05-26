b6bb21dff1cf9489047e938c2cccd486d1846ebb
Mohammed-Ali Hussein <mohammedalihussein02@gmail.com>
2022-05-26 16:45:58 +0800

    Talking to the server and returning the project data

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -4,6 +4,7 @@
 	import Projects from "./sections/Projects.svelte";
 	import Education from "./sections/Education.svelte";
 	import { onMount } from "svelte";
+	import axios from "axios";
 
 	let ready = false;
 	let projectData = [];
@@ -22,37 +23,15 @@
 		}
 	});
 
-	onMount(() => {
-		projectData = [
-			{
-				title: "Graph Path Finding Simulation",
-				description: "Path finding simulation implementing all data structures and algorithms manually. Thoroughly tested all classes and algorithms using JUnit. Achieved a final mark of 95%. [Mean mark across all campuses: 58%]",
-				technologies: ["Java", "Gradle", "JUnit", "Git"],
-				github: "",
-				link: "",
-			},
-			{
-				title: "Single Player Maze Survival Game",
-				description: "Single player maze traversal game with a basic artifical intelligence enemy. The player traverses the maze with a range of visibility radii for increased difficulty.",
-				technologies: ["C", "Make"],
-				github: "https://github.com/MohammedAliHussein/Maze-Survival-Game",
-				link: "",
-			},
-			{
-				title: "Personal Site V1",
-				description: "First iteration of my site. Learned the basics of web development practices, networking mechanisms and languages.",
-				technologies: ["HTML", "SCSS", "JavaScript", "RollupJS", "Express", "NodeJS", "NPM", "Git"],
-				github: "https://github.com/MohammedAliHussein/personal-site-v1",
-				link: "",
-			},
-			{
-				title: "Disk Scheduler Visualisation",
-				description: "Visual simulation of disk scheduling algorithms with seek time calculations.",
-				technologies: ["Svelte", "CSS", "RollupJS", "Express", "NodeJS", "NPM", "Git"],
-				github: "https://github.com/MohammedAliHussein/disk-scheduling-site",
-				link: "",
-			}
-		];
+	onMount(async () => {
+		let bulkData = (await axios.get("http://localhost:3000/api/v1/projects")).data;
+
+		let keys = Object.keys(bulkData);
+
+		keys.forEach((key) => {
+			projectData.push(bulkData[key].project);
+		});
+
 		ready = true;
 	});
 
